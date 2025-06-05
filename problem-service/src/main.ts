567cf3d6df7514a757fe9ab91570222355919076
emberow <chin45630@gmail.com>
2025-06-05 11:50:59 +0800

    feature: add dotenv

--- problem-service/src/main.ts
+++ problem-service/src/main.ts
@@ -1,19 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger 路徑 http://localhost:3000/api
   const config = new DocumentBuilder()
     .setTitle('My API')
     .setDescription('The API description')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // ← 預設路徑就是 /api
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
