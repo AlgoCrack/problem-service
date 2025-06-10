593a3b89d0b0ca76d7c3b443cbf1045664403700
emberow <chin45630@gmail.com>
2025-06-10 10:24:55 +0800

    feat: add prisma ORM package

--- problem-service/src/main.ts
+++ problem-service/src/main.ts
@@ -16,6 +16,12 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    `Application is running on: http://localhost:${process.env.PORT ?? 3000}`,
+  );
+  console.log(
+    `Swagger is running on: http://localhost:${process.env.PORT ?? 3000}/api`,
+  );
 }
 
 void bootstrap();
